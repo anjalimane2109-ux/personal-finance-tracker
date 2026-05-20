951737bdc0a9f2930985e70ae85198ae0bc31fab
anjalimane2109-ux <anjalimane2109@gmail.com>
2026-05-20 19:10:09 +0530

    fixed final eslint warning

--- finance-ui/src/context/AuthContext.js
+++ finance-ui/src/context/AuthContext.js
@@ -33,7 +33,7 @@ export const AuthProvider = ({ children }) => {
     // and does NOT call e.preventDefault()
     const loginUser = async ({ username, password }) => {
         try {
-            const response = await fetch('personal-finance-tracker-ku87.onrender.com/api/token/', {
+            const response = await fetch('https://personal-finance-tracker-ku87.onrender.com/api/token/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
