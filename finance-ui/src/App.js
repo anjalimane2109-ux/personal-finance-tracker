b3a1733c5a204c4dead5b0e5b0664f5ebffdf671
anjalimane2109-ux <anjalimane2109@gmail.com>
2026-05-20 19:46:54 +0530

    fixed final eslint warning

--- finance-ui/src/App.js
+++ finance-ui/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './components/Login';
-//import Signup from './components/Signup';
+import Signup from './components/Signup';
 import Home from './components/home';
 import Dashboard from './components/Dashboard';  // ⬅️ import this
 import PrivateRoute from './components/PrivateRoute';
-import RegisterPage from './components/Signup';
+
 
 function App() {
   return (
@@ -20,7 +20,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register" element={<Signup />} />
+          
           <Route
             path="/home"
             element={
