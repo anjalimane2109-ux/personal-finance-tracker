cc77e00dda3c740b6478a6d3031419e66a887a83
anjalimane2109-ux <anjalimane2109@gmail.com>
2026-05-20 19:38:15 +0530

    fixed final eslint warning

--- finance-ui/src/App.js
+++ finance-ui/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './components/Login';
-import Signup from './components/Signup';
+//import Signup from './components/Signup';
 import Home from './components/home';
 import Dashboard from './components/Dashboard';  // ⬅️ import this
 import PrivateRoute from './components/PrivateRoute';
